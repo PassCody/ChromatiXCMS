943dc69d9c9aa91d7e26da722583b649165acc11
PassCody <44557819+PassCody@users.noreply.github.com>
2019-08-20 15:20:06 +0200

    Add files via upload

---
+++ MC/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<?php
+	//require("../tools/anti_proxy.php");
+?>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Team ChromatiX - MC</title>
+		<link rel="shortcut icon" href="./imgs/Logo.png" />
+		<link rel="stylesheet" type="text/css" href="./css/Main.css">
+		
+		<!-- LIBRARY SCRIPTS -->
+		<script type="text/javascript" src="./tools/js/lib/jquery.min.js"></script>
+		
+		<!-- WORKER SCRIPTS -->
+		<script type="text/javascript" src="./tools/js/clock/clock.js"></script>
+		<script type="text/javascript" src="./tools/js/cookie-hint/cookie-hint.js"></script>
+		
+	</head>
+	
+	<body>
+		<div class="web_page" id="web_page" name="web_page">
+			<div class="web_header" id="web_header" name="web_header">
+				<div class="web_logo" id="web_logo" name="web_logo"></div>
+			</div>
+			<div class="web_top_menue" id="web_top_menue" name="web_top_menue">
+				<ul>
+					<li>
+						Home
+					</li>
+				</ul>
+				<ul>
+					<li>
+						Rules
+					</li>
+				</ul>
+				<ul>
+					<li>
+						Register
+					</li>
+				</ul>
+				<ul>
+					<li>
+						Forum
+					</li>
+				</ul>
+				<ul>
+					<li>
+						Maps
+					</li>
+				</ul>
+				<ul>
+					<li>
+						Skins
+					</li>
+				</ul>
+				<ul>
+					<li>
+						Guides
+					</li>
+				</ul>
+			</div>
+			<div class="web_content" id="web_content" name="web_content">
+				<div class="contant" id="contant" name="contant">
+					
+				</div>
+				<div class="side_bar" id="side_bar" name="side_bar">
+					
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
