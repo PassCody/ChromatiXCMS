f71eb9992d66c668136264fff0d605f1a7fa72f3
PassCody <44557819+PassCody@users.noreply.github.com>
2019-10-08 12:58:59 +0200

    Update config.php

--- tools/config.php
+++ tools/config.php
@@ -1,20 +1,20 @@
 <?php
 /* General Stuff */
-	$d_name		= 'www.team-chromatix.com';	/* Domain */
-	$p_name		= 'Team-ChromatiX';				/* Project Name */
+	$d_name		= '';				/* Domain */
+	$p_name		= '';				/* Project Name */
 
 /* Database Connection */
-	$db_ip			= '127.0.0.1';				/* DataBase IP */
-	$db_port		= '3306';					/* DataBase Port */
-	$db_user 		= 'Admin';					/* DataBase User */
-	$db_password	= 'llooll123';				/* DataBase Password */
-	$db_name		= 'web_cms';				/* DataBase Name */
+	$db_ip		= '';				/* DataBase IP */
+	$db_port	= '';				/* DataBase Port */
+	$db_user 	= '';				/* DataBase User */
+	$db_password	= '';				/* DataBase Password */
+	$db_name	= '';				/* DataBase Name */
 	
 /* Priavate Stuff For Law Stuff */
-	$f_name 		= 'Christian';				/* First Name */
-	$l_name	    	= 'Liebel';					/* Last Name */
-	$street	    	= 'A.-Schweitzer-Str. 24';	/* Street and House Number */
-	$p_code	    	= '76703 Kraichtal';		/* Post Code & State*/
-	$land	    	= 'Germany';				/* Land */
-	$e_mail	    	= 'TheikosCS@web.de';		/* Contact E-Mail */
+	$f_name 	= '';				/* First Name */
+	$l_name	    	= '';				/* Last Name */
+	$street	    	= '';				/* Street and House Number */
+	$p_code	    	= '';				/* Post Code & State*/
+	$land	    	= '';				/* Land */
+	$e_mail	    	= '';				/* Contact E-Mail */
 ?>
