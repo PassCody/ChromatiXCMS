e4b6fd01ba11c17fb58cc9cc6ba7a6cf35ae81be
Christian Liebel <44557819+PassCody@users.noreply.github.com>
2023-08-21 16:56:40 +0200

    Delete SQLHandler.php

--- src/tools/php/SQLHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-    CLASS SQLHandler{
-        private $path;
-        FUNCTION main($super) {
-            $this->path = $super->getPath();
-            print_r($this->path);
-        }
-    }
-?>
